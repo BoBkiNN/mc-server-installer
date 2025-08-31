335193fb21d4134470879b9ec54ed6225585bc49
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-08-31 18:45:13 +0500

    add todo

--- app/generate_schema.py
+++ app/generate_schema.py
@@ -1,6 +1,7 @@
 from model import *
 import json
 
+# TODO move this to main to use registries, invoking using subcommand
 r = Manifest.model_json_schema()
 Path("manifest_schema.json").write_text(json.dumps(r, indent=2))
 print("Done")
